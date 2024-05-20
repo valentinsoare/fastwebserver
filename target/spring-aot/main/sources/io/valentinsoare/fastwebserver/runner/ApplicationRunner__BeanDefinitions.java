c9c345209076c6c265907515adbbc7a5986a57e5
valentinsoare <soarevalentinn@gmail.com>
2024-05-20 21:14:36 +0300

    Adding Dockerfile

---
+++ target/spring-aot/main/sources/io/valentinsoare/fastwebserver/runner/ApplicationRunner__BeanDefinitions.java
@@ -0,0 +1,30 @@
+package io.valentinsoare.fastwebserver.runner;
+
+import io.valentinsoare.fastwebserver.services.AsyncNetworkServer;
+import org.springframework.aot.generate.Generated;
+import org.springframework.beans.factory.aot.BeanInstanceSupplier;
+import org.springframework.beans.factory.config.BeanDefinition;
+import org.springframework.beans.factory.support.RootBeanDefinition;
+
+/**
+ * Bean definitions for {@link ApplicationRunner}.
+ */
+@Generated
+public class ApplicationRunner__BeanDefinitions {
+  /**
+   * Get the bean instance supplier for 'applicationRunner'.
+   */
+  private static BeanInstanceSupplier<ApplicationRunner> getApplicationRunnerInstanceSupplier() {
+    return BeanInstanceSupplier.<ApplicationRunner>forConstructor(AsyncNetworkServer.class)
+            .withGenerator((registeredBean, args) -> new ApplicationRunner(args.get(0)));
+  }
+
+  /**
+   * Get the bean definition for 'applicationRunner'.
+   */
+  public static BeanDefinition getApplicationRunnerBeanDefinition() {
+    RootBeanDefinition beanDefinition = new RootBeanDefinition(ApplicationRunner.class);
+    beanDefinition.setInstanceSupplier(getApplicationRunnerInstanceSupplier());
+    return beanDefinition;
+  }
+}
